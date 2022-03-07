77bfe49c80a7ea0bd436ccc042cec0aa1ff77f27
Populo <pxpulo@gmail.com>
2022-03-06 19:29:53 -0500

    probably one and only release

---
+++ FFXIVHouseBotGUI/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace FFXIVHouseBotGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelProcess = new System.Windows.Forms.Label();
+            this.timerProcessRefresh = new System.Timers.Timer();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.numericUpDownDelay = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxMode = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.timerProcessRefresh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDelay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Enabled = false;
+            this.buttonStart.Location = new System.Drawing.Point(13, 41);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(289, 31);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelProcess
+            // 
+            this.labelProcess.Location = new System.Drawing.Point(13, 75);
+            this.labelProcess.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelProcess.Name = "labelProcess";
+            this.labelProcess.Size = new System.Drawing.Size(289, 21);
+            this.labelProcess.TabIndex = 1;
+            this.labelProcess.Text = "Process: ProcessName (Id) (click me if null)";
+            this.labelProcess.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelProcess.Click += new System.EventHandler(this.labelProcess_Click);
+            // 
+            // timerProcessRefresh
+            // 
+            this.timerProcessRefresh.Enabled = true;
+            this.timerProcessRefresh.Interval = 3000D;
+            this.timerProcessRefresh.SynchronizingObject = this;
+            this.timerProcessRefresh.Elapsed += new System.Timers.ElapsedEventHandler(this.timerProcessRefresh_Elapsed);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            // 
+            // numericUpDownDelay
+            // 
+            this.numericUpDownDelay.Location = new System.Drawing.Point(60, 12);
+            this.numericUpDownDelay.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDownDelay.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDownDelay.Name = "numericUpDownDelay";
+            this.numericUpDownDelay.Size = new System.Drawing.Size(72, 23);
+            this.numericUpDownDelay.TabIndex = 2;
+            this.numericUpDownDelay.Value = new decimal(new int[] {
+            450,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Delay:";
+            // 
+            // comboBoxMode
+            // 
+            this.comboBoxMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxMode.FormattingEnabled = true;
+            this.comboBoxMode.Items.AddRange(new object[] {
+            "Personal",
+            "FC",
+            "Relo Personal",
+            "Relo FC"});
+            this.comboBoxMode.Location = new System.Drawing.Point(138, 12);
+            this.comboBoxMode.Name = "comboBoxMode";
+            this.comboBoxMode.Size = new System.Drawing.Size(164, 23);
+            this.comboBoxMode.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 105);
+            this.Controls.Add(this.comboBoxMode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownDelay);
+            this.Controls.Add(this.labelProcess);
+            this.Controls.Add(this.buttonStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FFXIV House Bot";
+            ((System.ComponentModel.ISupportInitialize)(this.timerProcessRefresh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDelay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Timers.Timer timerProcessRefresh;
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label labelProcess;
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownDelay;
+        private System.Windows.Forms.ComboBox comboBoxMode;
+    }
+}
---
+++ FFXIVHouseBotGUI/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace FFXIVHouseBotGUI
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+
+        private Process _process { get; set; }
+
+        private bool _running = false;
+        
+        const UInt32 WM_KEYDOWN = 0x0100;
+        const UInt32 WM_KEYUP = 0x0101;
+
+        const int VK_NUMZERO = 0x60;
+        const int VK_NUMTWO = 0x62;
+        const int VK_NUMFOUR = 0x64;
+        const int VK_NUMEIGHT = 0x68;
+
+        List<int> Keystrokes;
+
+        List<int> buyPersonal = new()
+        {
+            // select placard
+            VK_NUMZERO,
+            // open placard
+            VK_NUMZERO,
+            // click purchase
+            VK_NUMZERO,
+            // click personal
+            VK_NUMZERO,
+            // move left to Yes
+            VK_NUMFOUR,
+            // click yes
+            VK_NUMZERO
+        };
+
+        List<int> buyFC = new()
+        {
+            // select placard
+            VK_NUMZERO,
+            // open placard
+            VK_NUMZERO,
+            // click purchase
+            VK_NUMZERO,
+            // select FC
+            VK_NUMTWO,
+            // click FC
+            VK_NUMZERO,
+            // move left to Yes
+            VK_NUMFOUR,
+            // click yes
+            VK_NUMZERO
+        };
+
+        List<int> reloPersonal = new()
+        {
+            // select placard
+            VK_NUMZERO,
+            // open placard
+            VK_NUMZERO,
+            // click purchase
+            VK_NUMZERO,
+            // select FC
+            VK_NUMTWO,
+            // select Relo Personal
+            VK_NUMTWO,
+            // click relo personal
+            VK_NUMZERO,
+            // move left to Yes
+            VK_NUMFOUR,
+            // click yes
+            VK_NUMZERO
+        };
+
+        List<int> reloFC = new()
+        {
+            // select placard
+            VK_NUMZERO,
+            // open placard
+            VK_NUMZERO,
+            // click purchase
+            VK_NUMZERO,
+            // select Relo FC
+            VK_NUMEIGHT,
+            // click relo FC
+            VK_NUMZERO,
+            // move left to Yes
+            VK_NUMFOUR,
+            // click yes
+            VK_NUMZERO
+        };
+
+        public Form1()
+        {            
+            InitializeComponent();
+
+            comboBoxMode.SelectedIndex = 0;
+
+            HandleProcess();
+
+            Keystrokes = new List<int>();
+
+            backgroundWorker.DoWork += WorkerOnDoWork;
+        }
+
+        private void WorkerOnDoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            
+            if (null != _process && !_process.HasExited)
+            {
+                while (!worker.CancellationPending)
+                {
+                    RunBot();
+                }
+            }
+        }
+
+        private Process FindProcess()
+        {
+            Process proc = Process.GetProcesses()
+                .Where(p => p.ProcessName != typeof(Form1).Assembly.GetName().Name)
+                .FirstOrDefault(p => p.ProcessName.StartsWith("ffxiv", StringComparison.CurrentCultureIgnoreCase));
+
+            return proc;
+        }
+
+        private void timerProcessRefresh_Elapsed(object sender, ElapsedEventArgs e) => HandleProcess();
+        private void labelProcess_Click(object sender, EventArgs e) => HandleProcess();
+
+        private void HandleProcess()
+        {
+            var proc = FindProcess();
+
+            if (null != proc)
+            {
+                _process = proc;
+                buttonStart.Enabled = true;
+                labelProcess.Text = $"Process: {_process.ProcessName} ({_process.Id})";
+            }
+            else
+            {
+                _process = null;
+                buttonStart.Enabled = false;
+                labelProcess.Text = "Click me to find process";
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // need to select mode
+            switch (comboBoxMode.SelectedItem as string)
+            {
+                case "Personal":
+                    Keystrokes = buyPersonal;
+                    break;
+                case "FC":
+                    Keystrokes = buyFC;
+                    break;
+                case "Relo Personal":
+                    Keystrokes = reloPersonal;
+                    break;
+                case "Relo FC":
+                    Keystrokes = reloFC;
+                    break;
+            }
+
+            _running = !_running;
+
+            if (_running)
+            {
+                buttonStart.Text = "Stop";
+                backgroundWorker.RunWorkerAsync();
+            }
+            else
+            {
+                buttonStart.Text = $"Start";
+                backgroundWorker.CancelAsync();
+            }
+        }
+
+        private void RunBot()
+        {
+            int sleep = (int)numericUpDownDelay.Value;
+
+            while (_running)
+            {
+                foreach(var key in Keystrokes)
+                {
+                    PressKey(key);
+                    Thread.Sleep(sleep);
+                }
+                // extra delay between attempts for stability
+                Thread.Sleep(sleep);
+            }
+        }
+
+        private void PressKey(int key)
+        {
+            PostMessage(_process.MainWindowHandle, WM_KEYDOWN, key, 0);
+            //Debug.WriteLine("down");
+            Thread.Sleep(25);
+            PostMessage(_process.MainWindowHandle, WM_KEYUP, key, 3);
+            //Debug.WriteLine("up");
+        } 
+    }
+}
